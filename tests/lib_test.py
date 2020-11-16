3fd8ae37546b31b9011626ed2215371ca6aff51e
Alvaro Pata <helmanrm@gmail.com>
2020-11-16 14:28:39 +0100

    first commit

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -5,15 +5,9 @@ import os
 import toolkit
 import pandas as pd
 # Import from our lib
-from toolkit.lib import clean_data
+from toolkit.lib import *
 import pytest
 
-
-def test_clean_data():
-    datapath = os.path.dirname(os.path.abspath(toolkit.__file__)) + '/data'
-    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
-    first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
-    assert list(df.columns)[:6] == first_cols
-    assert df.shape == (999, 142)
-    out = clean_data(df)
-    assert out.shape == (985, 119)
+def test_fetch_url():
+    url = 'http://iex.lewagon.com/stable/stock/'
+    assert type(fetch_url(url)) == 'pandas.core.frame.DataFrame'
